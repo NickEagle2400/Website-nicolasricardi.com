7e7f227c6b15e5572c9393d0bef111bea864d43e
Nicolas Ricardi <nickricardi00@gmail.com>
2023-01-14 18:00:24 +0100

    ActiveCampaing Newsletter form

--- components/NewsletterForm.jsx
+++ components/NewsletterForm.jsx
@@ -1,46 +1,49 @@
 import { useState } from "react";
 
-const NewsletterForm = () =>{
+const NewsletterForm = () => {
 
-    const [inputEmail, setInputEmail] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputName, setInputName] = useState("");
 
-    const handleChange = (e) => {
-        setInputEmail(e.target.value);
-    }
+  const handleEmailChange = (e) => {
+    setInputEmail(e.target.value);
+  }
+  const handleNameChange = (e) => {
+    setInputName(e.target.value);
+  }
 
-    const handleSubmit = (e) => {
-        alert(inputEmail);
+  return (
+    <form method="POST" action={process.env.ACTIVECAMPAIGN_URL_FORM} id="_form_1_" noValidate>
+      <input type="hidden" name="u" value="1" />
+      <input type="hidden" name="f" value="1" />
+      <input type="hidden" name="s" />
+      <input type="hidden" name="c" value="0" />
+      <input type="hidden" name="m" value="0" />
+      <input type="hidden" name="act" value="sub" />
+      <input type="hidden" name="v" value="2" />
+      <input type="hidden" name="or" value="d14622a2a8148b9cbe10cce53da6e6ac" />
 
-        const options = {
-            method: 'POST',
-            headers: {accept: 'application/json', 'content-type': 'application/json'},
-            body: JSON.stringify({
-                contact: {
-                    email: {inputEmail},
-                    firstName: 'John',
-                    lastName: 'Doe',
-                    phone: '7223224241'
-                }
-            })
-        };
-        
-        fetch('https://nicolasricardi.api-us1.com/api/3/contacts', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
+      <div className="_form-content">
 
-        e.preventDefault();
-    }
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="w-full lg:w-1/2" >
+              <input type="text" value={inputName} onChange={handleNameChange} id="fullname" name="fullname" placeholder="Nome" required className="bg-zinc-800 px-4 py-3 lg:py-2 text-gray-200 block rounded outline-none border-[2px] border-transparent w-full font-light focus:border-zinc-600" />
+          </div>
+          <div className="w-full lg:w-1/2" >
+              <input type="text" value={inputEmail} onChange={handleEmailChange} id="email" name="email" placeholder="La tua miglior email" required className="bg-zinc-800 px-4 py-3 lg:py-2 text-gray-200 block rounded outline-none border-[2px] border-transparent w-full font-light focus:border-zinc-600" />
+          </div>
+        </div>
 
+        <div className="mt-6">
+          <button id="_form_1_submit" className="w-full bg-white py-3 rounded font-medium transition-transform duration-200 hover:scale-105" type="submit">
+            Si, voglio ricereve la Checklist gratuita
+          </button>
+        </div>
 
-    return(
-        <form onSubmit={handleSubmit}>
-            <div className="flex flex-row">
-                <input type="email" value={inputEmail} onChange={handleChange} required placeholder="La tua miglior email" className="bg-zinc-800 px-3 py-3 text-gray-200 block rounded-l-md outline-none w-3/5 border-[1px] border-white font-light" />
-                <button type="submit" className="block rounded-r-md bg-white text-black px-4 font-bold w-2/5">Invia la checklist</button>
-            </div>
-        </form>
-    )
+      </div>
+    </form>
+  )
 }
 
 export default NewsletterForm
+

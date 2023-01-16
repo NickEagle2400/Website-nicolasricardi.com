60c2eec06f5de639cfd4b8c53b58bc20cbcfafc8
Nicolas Ricardi <nickricardi00@gmail.com>
2023-01-16 22:12:08 +0100

    Cookie Banner Iubenda

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -21,6 +21,19 @@ const Document = () => {
                         `,
                     }}
                 />
+
+                {/*IUBENDA*/}
+                <script type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            var _iub = _iub || [];
+                            _iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"ccpaAcknowledgeOnDisplay":true,"consentOnContinuedBrowsing":false,"countryDetection":true,"enableCcpa":true,"enableLgpd":true,"invalidateConsentWithoutLog":true,"lgpdAppliesGlobally":false,"perPurposeConsent":true,"siteId":2800016,"cookiePolicyId":32129616,"lang":"it", "banner":{ "acceptButtonColor":"#188CF9","acceptButtonDisplay":true,"backgroundColor":"#0D0F11","backgroundOverlay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"explicitWithdrawal":true,"fontSizeBody":"12px","listPurposes":true,"position":"float-bottom-center","rejectButtonColor":"#188CF9","rejectButtonDisplay":true }};
+                        `,
+                    }}
+                />
+                <script type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js" />
+                <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async />
+
             </Head>
             <body>
                 <Main />

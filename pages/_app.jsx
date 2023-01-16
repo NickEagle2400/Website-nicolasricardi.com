6fedb7b6c1965a3713a7cdd87170fffe32cc0608
Nicolas Ricardi <nickricardi00@gmail.com>
2023-01-16 22:08:32 +0100

    Connessione a GA4

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,10 +3,31 @@ import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/react';
 import "@fortawesome/fontawesome-svg-core/styles.css"; 
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
 
 config.autoAddCss = false; 
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
     <>
       <Component {...pageProps} />

3b78b20f8a1d11ef63a60d6705a7c0c0490e5224
Nicolas Ricardi <nickricardi00@gmail.com>
2023-02-02 17:47:34 +0100

    Update testimonial

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -64,8 +64,8 @@ const Testimonials = () => {
                                             <Image src={RevSecond} fill alt="test" style={{objectFit:"cover"}}/>
                                         </div>
                                         <div>
-                                            <p className="text-gray-300 cursor-default font-medium">Antonio La Falce</p>
-                                            <p className="text-gray-300 cursor-default font-medium">CEO Hintime Group</p>
+                                            <p className="text-gray-300 cursor-default font-medium">Mauro Rossi</p>
+                                            <p className="text-gray-300 cursor-default font-medium">CEO</p>
                                         </div>
                                     </div>
                                     
